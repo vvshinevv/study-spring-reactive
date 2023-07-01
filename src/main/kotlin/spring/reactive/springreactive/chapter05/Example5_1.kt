ad82976ba7a6bf8d330fccd616064b07adb2e641
최홍희 <vvshinevv@gmail.com>
2023-07-01 16:55:57 +0900

    샘플 코드 따라해보기

--- src/main/kotlin/spring/reactive/springreactive/chapter05/Example5_1.kt
+++ src/main/kotlin/spring/reactive/springreactive/chapter05/Example5_1.kt
@@ -1,8 +1,10 @@
 package spring.reactive.springreactive.chapter05
 
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 fun main() {
     val sequence = Flux.just("hello", "reactor")
     sequence.map { it.uppercase() }.subscribe { println(it) }
+    Mono.justOrEmpty("a").concatWith()
 }
