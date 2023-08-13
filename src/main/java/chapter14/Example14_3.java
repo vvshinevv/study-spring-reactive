869c160604a7de7801414dab83938d9f8268af8a
hongs.choi <vvshinevv@gmail.com>
2023-08-13 18:15:26 +0900

    14_3예제 추가

---
+++ src/main/java/chapter14/Example14_3.java
@@ -0,0 +1,15 @@
+package chapter14;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.util.stream.Stream;
+
+@Slf4j
+public class Example14_3 {
+    public static void main(String[] args) {
+        Flux.fromStream(() -> Stream.of("BTC", "DOGE"))
+                .filter(coin -> coin.equals("BTC") || coin.equals("ETH"))
+                .subscribe(data -> log.info("{}", data));
+    }
+}
